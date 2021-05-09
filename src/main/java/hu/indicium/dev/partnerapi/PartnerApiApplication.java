4e3829c24b909fddf92d33af3bbfccee0b83f070
joostlek <joostlek@outlook.com>
2021-05-09 19:12:24 +0200

    Initial commit

---
+++ src/main/java/hu/indicium/dev/partnerapi/PartnerApiApplication.java
@@ -0,0 +1,13 @@
+package hu.indicium.dev.partnerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/indicium/dev/partnerapi/PartnerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.indicium.dev.partnerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
